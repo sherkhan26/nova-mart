2b042b29f6aca83e42ca874c926b00a3fe1ecb13
Шерхан Амангелды <sherhan_26@mail.ru>
2025-04-16 19:37:32 +0500

    update

---
+++ client-side/app/components/layout/header/search/Search.tsx
@@ -0,0 +1,35 @@
+import { SearchIcon } from '@chakra-ui/icons'
+import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
+import { FC, useState } from 'react'
+
+import styles from './Search.module.scss'
+
+const Search: FC = () => {
+	const [searchTerm, setSearchTerm] = useState('')
+
+	return (
+		<div className={styles.search}>
+			<InputGroup>
+				<InputLeftElement
+					pointerEvents='none'
+					children={<SearchIcon color='gray.500' />}
+				/>
+				<Input
+					variant='flushed'
+					type='search'
+					onChange={e => setSearchTerm(e.target.value)}
+					value={searchTerm}
+					placeholder='Search'
+					_focus={{
+						boxShadow: 'none'
+					}}
+					_focusVisible={{
+						borderColor: '#008D64'
+					}}
+				/>
+			</InputGroup>
+		</div>
+	)
+}
+
+export default Search
