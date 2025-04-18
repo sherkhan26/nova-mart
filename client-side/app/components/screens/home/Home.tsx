7e150757bd12f980dff698e811721c1496186bc0
Шерхан Амангелды <sherhan_26@mail.ru>
2025-04-18 19:37:32 +0500

    update

---
+++ client-side/app/components/screens/home/Home.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react'
+
+import Layout from '@/layout/Layout'
+
+import Catalog from '@/ui/catalog/Catalog'
+import Heading from '@/ui/heading/Heading'
+
+import { IProductsPage } from '@/../pages'
+
+const Home: FC<IProductsPage> = ({ products }) => {
+	return (
+		<Layout
+			title='Home'
+			description='More than just great coffee. Explore the menu, sign up for Starbucks® Rewards, manage your gift card and more.'
+		>
+			<Heading className='text-right'>
+				The happiest hour of the year RED Group
+			</Heading>
+			<Catalog products={products} />
+		</Layout>
+	)
+}
+
+export default Home
